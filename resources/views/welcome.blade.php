948296f7972d498342d86336419b98045c4b1f32
Blackmarch <blackmoment03@gmail.com>
2023-03-19 01:34:55 +0700

    tambah file baru

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio | Jefrry Arispratama</title>
+
+    
+    <link href="{{url('style/bootstrap.css')}}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{url('style/style.css')}}">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+
+<body id="home">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-success navbar-dark bg-opacity-100 shadow-sm fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Jefrry Arispratama</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- navbar -->
+
+    <!-- jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="{{url('img/Jefrry Arispratama.jpeg')}}" alt="Jefrry Arispratama" width="200px"
+            class="rounded-circle img-thumbnail">
+        <h1 class="display-4">Jefrry Arispratama</h1>
+        <p class="lead">Collage Student | Freelancer</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#fff" fill-opacity="1"
+                d="M0,160L48,144C96,128,192,96,288,106.7C384,117,480,171,576,197.3C672,224,768,224,864,218.7C960,213,1056,203,1152,186.7C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+
+    <!-- jumbotron -->
+
+    <!-- About -->
+    <section id="about">
+        <div class="container">
+            <div class="row text-center mb-5">
+                <div class="col">
+                    <h2>About Me</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center fs-6 text-center ">
+                <div class="col-md-5">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam neque, aut, illum eius vero autem
+                        laboriosam aliquam harum dicta earum aperiam doloribus laborum odit tempora iusto adipisci quis
+                        sit suscipit.</p>
+                </div>
+
+                <div class="col-md-5">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ratione reiciendis fuga
+                        quod porro et eos autem sit. Ipsum vitae doloremque odit, suscipit deserunt tempora vero ea
+                        corrupti tempore ex.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#D1E7DD" fill-opacity="1"
+                d="M0,64L48,96C96,128,192,192,288,186.7C384,181,480,107,576,106.7C672,107,768,181,864,224C960,267,1056,277,1152,256C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+
+    </section>
+
+    <!-- About -->
+
+    <!-- Projects -->
+
+    <section id="projects">
+        <div class="container">
+            <div class="row text-center mb-5">
+                <div class="col">
+                    <h2>
+                        My Projects
+                    </h2>
+                </div>
+            </div>
+
+            <div class="row mb-3 justify-content-center">
+                <div class="col-md-4 mb-5">
+                    <div class="card">
+                        <img src="{{url('img/sertifikat 1.jpg')}}" class="card-img-top" alt="sertifikat 1">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 mb-5">
+                    <div class="card">
+                        <img src="{{url('img/sertifikat 2.jpg')}}" class="card-img-top" alt="sertifikat 1">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 mb-5">
+                    <div class="card">
+                        <img src="{{url('img/sertifikat 3.jpg')}}" class="card-img-top" alt="sertifikat 1">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+            <div class="row mb-3 justify-content-center">
+                <div class="col-md-4 mb-5">
+                    <div class="card">
+                        <img src="{{url('img/sertifikat 3.jpg')}}" class="card-img-top" alt="sertifikat 1">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 mb-5">
+                    <div class="card">
+                        <img src="{{url('img/sertifikat 1.jpg')}}" class="card-img-top" alt="sertifikat 1">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 mb-5">
+                    <div class="card">
+                        <img src="{{url('img/sertifikat 2.jpg')}}" class="card-img-top" alt="sertifikat 1">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#fff" fill-opacity="1"
+                d="M0,224L48,213.3C96,203,192,181,288,170.7C384,160,480,160,576,176C672,192,768,224,864,224C960,224,1056,192,1152,176C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+
+    </section>
+
+    <!-- Projects -->
+
+    <!-- contact -->
+
+    <section id="contact">
+        <div class="container mb-5">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h2>
+                        Contact Me
+                    </h2>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-md-7">
+                    <form>
+                        <div class="mb-3">
+                            <label for="Name" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="Pesan" class="form-label">Pesan</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-success">Kirim</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#198754" fill-opacity="1"
+                d="M0,32L48,37.3C96,43,192,53,288,69.3C384,85,480,107,576,133.3C672,160,768,192,864,186.7C960,181,1056,139,1152,128C1248,117,1344,139,1392,149.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+
+    <!-- contact -->
+
+
+    <!-- footer -->
+
+    <footer class="bg-success text-white text-center pb-5">
+        <p>
+            Created with <i class="bi bi-suit-heart text-danger"></i> by <a
+                href="https://www.instagram.com/blackmrch_03/" class="text-white fw-bold ">Jefrry
+                Arispratama</a> </p>
+    </footer>
+
+    <!-- footer -->
+
+
+
+
+
+
+
+
+
+    <script src="{{url('js/bootstrap.js')}}"></script>
+</body>
+
+</html>
